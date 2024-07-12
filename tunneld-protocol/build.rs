313a6890eed6d382f9682a5edc971d68e0dedf4d
sword-jin <sword@cybozu.co.jp>
2024-07-12 08:22:22 +0000

    remove strip

--- tunneld-protocol/build.rs
+++ tunneld-protocol/build.rs
@@ -1,8 +1,24 @@
 use std::io::Result;
 
+use which::which;
+
 fn main() -> Result<()> {
-    tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["src/message.proto"], &["src/"])?;
+    let builder = tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional");
+
+    match which("protoc") {
+        Ok(_) => {
+            println!("found protoc");
+            builder.compile(&["src/message.proto"], &["src/"])?;
+        }
+        Err(_) => {
+            println!("since there is no protoc in the path, we skip the protoc run");
+            println!("protoc --descriptor_set_out=message.bin --proto_path=. message.proto");
+            builder
+                .skip_protoc_run()
+                .file_descriptor_set_path("src/message.bin")
+                .compile(&["src/message.bin"], &["src/"])?;
+        }
+    }
+
     Ok(())
 }
