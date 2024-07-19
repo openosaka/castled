b8644d285349c8fa3acad5420520d20783b892c1
sword-jin <sword@cybozu.co.jp>
2024-07-19 08:51:48 +0000

    add go in ci:

--- tests/e2e/test_udp_tunnel_to_google_dns.sh
+++ tests/e2e/test_udp_tunnel_to_google_dns.sh
@@ -13,11 +13,11 @@ cleanup() {
 
 trap cleanup EXIT
 
-exec ./target/debug/castled &
+exec $root_dir/target/debug/castled &
 server_pid=$!
 wait_port 6610
 
-exec ./target/debug/castle udp 53 --remote-port 10053 --local-addr 8.8.8.8 &
+exec $root_dir/target/debug/castle udp 53 --remote-port 10053 --local-addr 8.8.8.8 &
 client_pid=$!
 wait_port_on_udp 10053
 
